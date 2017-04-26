f8ef35a3e46fe966359ebcb56b1a536cb40931c7
terryc0158 <terryc0158@my.uwstout.edu>
2017-04-26 14:41:21 -0500

    start to a username

--- index.js
+++ index.js
@@ -8,10 +8,9 @@ app.get('/', function(req, res) {
 
 io.on('connection', function(socket) {
   socket.on('chat message', function(msg) {
-    io.emit('chat message', msg);
+    io.emit('chat message', socket.id, msg);
   });
 });
-
 http.listen(3000, function() {
   console.log('listening on *:3000');
 });
